3b2eaea7981212b70165d57015a6f820efa93d4d
Juan Cazala <cazala@mail.sfsu.edu>
2017-04-04 10:44:07 -0300

    memoize-selectors

--- src/selectors.js
+++ src/selectors.js
@@ -1,22 +1,22 @@
+import { createSelector } from 'reselect'
+
 // selectors
 export const getBooks = (state) => state.books
 export const getAuthors = (state) => state.authors
 export const getUI = (state) => state.ui
 
 // compute book list
-export const getList = (state) => {
-
-  const books = getBooks(state)
-  const authors = getAuthors(state)
-  const ui = getUI(state)
-
-  return Object.keys(books)
-  .map(bookId => {
-    const book = books[bookId]
-    return {
-      ...book,
-      author: authors[book.authorId],
-      selected: ui.selected === book.id
-    }
-  })
-}
+export const getList = createSelector(
+  getBooks,
+  getAuthors,
+  getUI,
+  (books, authors, ui) => Object.keys(books)
+    .map(bookId => {
+      const book = books[bookId]
+      return {
+        ...book,
+        author: authors[book.authorId],
+        selected: ui.selected === book.id
+      }
+    })
+)
